e372a2e8ab37041b9b45416255319f18678e02dd
welquerjunio <welquerrodrigues.05@gmail.com>
2023-12-03 12:16:26 -0300

    usando api

--- pokeweb/src/pages/Home.jsx
+++ pokeweb/src/pages/Home.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { Grid } from "@mui/material";
+import { Box, Container } from "@mui/system";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Navbar from "../componets/Navbar";
+import PokemonCard from "../componets/PokemonCard";
 
 export const Home = () => {
+
+        const [pokemons, setPokemons] = useState([]);
+
+        useEffect(() => {
+            getPokemons();
+        }, []);
+
+        const getPokemons = () => {
+            axios
+            .get("https://pokeapi.co/api/v2/pokemon?limit=50")
+            .then((res) => setPokemons(res.data.results))
+            .catch((err) => console.log(err));
+        };
+ 
     return(
-        <div>Home</div>
+        <div>
+            <Navbar />
+
+            <Container maxWidth="false">
+                <Grid container>
+                    {pokemons.map((pokemon, key) => (
+                        <Grid item xs={3} key={key}>
+                             <PokemonCard name={pokemon.name} />
+                        </Grid>   
+
+                    ))}        
+                </Grid>
+            </Container>
+
+        </div>
     )
 }
